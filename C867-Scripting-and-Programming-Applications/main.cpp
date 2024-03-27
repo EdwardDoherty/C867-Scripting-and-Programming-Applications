3f6a7ccf07ef2d8f738fbcb783cdb35482bd57dd
Edward Doherty <109316023+EdwardDoherty@users.noreply.github.com>
2024-03-26 19:11:35 -0700

    Remove studentData from main.cpp

--- C867-Scripting-and-Programming-Applications/main.cpp
+++ C867-Scripting-and-Programming-Applications/main.cpp
@@ -29,19 +29,10 @@ int main()
     std::cout << "Course: " << courseTitle <<
         "\n Programming Language Used: " << programmingLanguage <<
         "\n WGU Student ID: " << WGUstudentID <<
-        "\n WGU Student Name: " << WGUstudentName;
+        "\n WGU Student Name: " << WGUstudentName << std::endl;
 
 
-    // A. Modify the “studentData Table” to include your personal information as the last item.
-    const std::string studentData[] =
-    {
-        "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
-        "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
-        "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
-        "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-        "A5,Joseph,Doherty,jdohe23@gmail.com,27,16,2,33,SOFTWARE"
-    };
-
+    Roster testRoster;
 
         //classRoster.printAll();
 
